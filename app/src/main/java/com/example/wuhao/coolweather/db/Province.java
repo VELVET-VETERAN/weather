8fc3a589f2948a8f3768672224fbc24767443d52
wuhao <395481138@qq.com>
2017-12-14 16:17:27 +0800

    加入数据库和表配置

---
+++ app/src/main/java/com/example/wuhao/coolweather/db/Province.java
@@ -0,0 +1,39 @@
+package com.example.wuhao.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by wuhao on 2017/12/14.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId(){
+        return id;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
